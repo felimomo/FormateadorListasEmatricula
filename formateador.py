8304e058391da76a29406e78675b7157bd750f0b
felimomo <felimomouni@gmail.com>
2026-08-13 08:52:19 -0600

    script comments

--- formateador.py
+++ formateador.py
@@ -18,18 +18,7 @@ def format(
 		"Output" / 
 		out_file_name
 	)
-	# Antes lo hacía por medio de un archivo de texto, pero
-	# ahora debería funcionar de una forma más estable.
 	#
-	# in_file_txt = in_file.stem+".txt"
-	# (
-	# 	pd.read_excel(in_file, usecols=columnas)
-	# 		.to_csv(in_file_txt, sep='\t', index=False)
-	# )
-	# with open(in_file_txt, 'r') as f, out_file.open('w') as o:
-	# 	for line in f:
-	# 		if line.strip():
-	# 			o.write(line)
 	(
 		pd.read_excel(in_file, usecols=columnas)
 		.dropna(how='any')
